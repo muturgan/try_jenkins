7a445d1697ce9b713b9d081e0b72fe5c2e6057f1
Andrey Sakharov <a.sakharov@biletix.ru>
2020-04-05 15:12:00 +0300

    a predeploy script was added

---
+++ jest.config-for-js.js
@@ -0,0 +1,12 @@
+module.exports = {
+  moduleFileExtensions: [
+    'js',
+    'json'
+  ],
+  testMatch: [
+    '**/dist/tests/**/*.test.(js)',
+    '**/dist/tests/**/*.spec.(js)'
+  ],
+  testEnvironment: 'node'
+};
+  
